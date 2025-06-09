339e4e2ff789c5a2e4e6f938dfe96b960586db2a
maoriab <maorimichell788@gmail.com>
2025-06-09 16:57:15 -0500

    paginador

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 const paramsString = window.location.search;
 const searchParams = new URLSearchParams(paramsString);
-fetch("https://rickandmortyapi.com/api/character")
+
+const pageUrl = searchParams.get("page");
+
+let paramsPagination = "";
+if (pageUrl !== 1 || pageUrl !== null) {
+    paramsPagination = `?page=${pageUrl}`;
+}
+
+
+fetch("https://rickandmortyapi.com/api/character" + paramsPagination)
 .then(response => response.json())
 .then(json => {
     console.log(json)
     const container = document.querySelector(".container-uno")
+    
+    const pagination = document.querySelector("#pagination")
+    
+    let pages = ""
+    for (let pageIndex = 1; pageIndex <= json.info.pages; pageIndex++) {
+        const page = `<a href="?page=${pageIndex}">${pageIndex}</a>`      
+        pages += page
+    }
+    pagination.innerHTML = pages
+
     let containerData = ""
     json.results.forEach(character => {
         const card = `
